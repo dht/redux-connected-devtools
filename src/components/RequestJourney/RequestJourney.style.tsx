87baca381d96469a54ce992c56550eb1db45ae39
Guy <91410132+guy-st@users.noreply.github.com>
2022-03-14 11:23:35 +0200

    wip

--- src/components/RequestJourney/RequestJourney.style.tsx
+++ src/components/RequestJourney/RequestJourney.style.tsx
@@ -3,4 +3,25 @@ import styled from 'styled-components';
 export const Container = styled.div`
     flex: 1;
     border-top: 1px solid #333;
+    max-height: 380px;
+    overflow-y: scroll;
+
+    &::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    /* Track */
+    &::-webkit-scrollbar-track {
+        background: #333;
+    }
+
+    /* Handle */
+    &::-webkit-scrollbar-thumb {
+        background: #555;
+    }
+
+    /* Handle on hover */
+    &::-webkit-scrollbar-thumb:hover {
+        background: #666;
+    }
 `;
