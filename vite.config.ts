6a6c5982cd9b0ca6c93d5f6df62ae9e8e4c7203b
dht <dht@users.noreply.github.com>
2022-09-03 03:35:33 +0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,27 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-const svgLoader = require('vite-plugin-react-svg');
-const path = require('path');
-require('dotenv-flow').config();
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import analyze from 'rollup-plugin-analyzer';
+import p from './package.json';
+import { externals } from 'shared-base';
 
-const cwd = path.resolve(process.cwd(), '../../');
-const host = (process.env.VITE_SERVER_DOMAIN || '').replace(/https?:\/\//, '');
-
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        tsconfigPaths({
-            loose: true,
-        }),
-        react(),
-        svgLoader(),
-    ],
-    define: {
-        'process.env.MONO': process.env.MONO,
-    },
-    resolve: {
-        alias: {
-            '@gli/forms': `${cwd}/packages/web-forms/src`,
-            '@gli/tables': `${cwd}/packages/web-tables/src`,
-            '@gli/details': `${cwd}/packages/web-details/src`,
-            '@gli/devtools': `${cwd}/packages/web-devtools/src`,
-            '@gli/router': `${cwd}/packages/web-router/src`,
-            '@gli/ui': `${cwd}/packages/web-ui/src`,
-            '@gli/base-ui': `${cwd}/packages/web-base-ui/src`,
-            '@gli/testing-base': `${cwd}/packages/web-testing-base/src`,
-            '@gli/hooks': `${cwd}/packages/hooks/src`,
-            '@gli/root-types': `${cwd}/packages/root-types/src`,
-            '@gli/themes': `${cwd}/packages/web-themes/src`,
-            '@gli/platformer': `${cwd}/packages/platformer`,
-            '@gli/app-home': `${cwd}/apps/home`,
+    plugins: [react(), dts({})],
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'RcDevtools',
+            formats: ['es', 'umd'],
+            fileName: (format) => `rc-devtools.${format}.js`,
         },
-    },
-    server: {
-        host,
-        port: 3001,
+        rollupOptions: {
+            plugins: [analyze()],
+            ...externals({
+                'react/jsx-runtime': '',
+                ...p.dependencies,
+            }),
+        },
     },
 });
