7581d49e830fde306f6520a44cb5c0a68118ae1b
dht <dht@users.noreply.github.com>
2022-09-17 06:39:18 +0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         rollupOptions: {
             plugins: [analyze()],
             ...externals({
+                react: '',
                 'react/jsx-runtime': '',
                 ...p.dependencies,
             }),
